1cb6d2d6893d382010d8baf4cd63774a8093555e
Arup_kr_das <arup80424@gmail.com>
2025-06-16 18:28:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+
+    // Auto close navbar on mobile after clicking link
+    document.querySelector('.nav-links').classList.remove('active');
+  });
+});
+// Scroll Animation for Project Cards
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+      entry.target.style.transition = "all 0.6s ease-out";
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+document.querySelectorAll('.project-card').forEach(card => {
+  observer.observe(card);
+});
+
+// Hamburger Toggle
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
